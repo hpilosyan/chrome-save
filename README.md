1b000a058104f35e4780b3bdcdfdb6d71e55467e
Hayk Pilosyan <hpilosyan@macadamian.com>
2014-09-11 22:42:13 +0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+Exposes console.save method for saving javascript object to a file.
+
+Source:
+http://stackoverflow.com/questions/11849562/how-to-save-the-output-of-a-console-logobject-to-a-file
---
+++ content.js
@@ -0,0 +1,10 @@
+(function(){
+
+var s = document.createElement('script');
+s.src = chrome.extension.getURL('script.js');
+s.onload = function() {
+    this.parentNode.removeChild(this);
+};
+(document.head||document.documentElement).appendChild(s);
+
+})();
